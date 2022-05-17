cc11485161dd006a14d0115785736cebf14990b6
joicereis <joice.jdosreis@gmail.com>
2022-05-16 22:13:16 -0300

    Meu primeiro commit Java

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato();
+		System.out.println(gato);
+
+	}
+}
